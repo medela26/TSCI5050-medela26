dffba61eca558f86e8e6973aaa7ceb6eb3bd5f2c
unknown <medela@livemail.uthscsa.edu>
2026-04-07 11:56:42 -0500

    Constellation obtaining, combining different data sets

---
+++ ExampleWorkflow.R
@@ -0,0 +1,101 @@
+#'---
+#' title: "TSCI 5050: Introduction to Data Science"
+#' author: 'AJ Medel ^1^'
+#' abstract: |
+#'  | Read source data and process it
+#' documentclass: article
+#' description: 'Manuscript'
+#' clean: false
+#' self_contained: true
+#' number_sections: false
+#' keep_md: true
+#' fig_caption: true
+#' output:
+#'  html_document:
+#'    toc: true
+#'    toc_float: true
+#'    code_folding: show
+#' ---
+#'
+#+ init, echo=FALSE, message=FALSE, warning=FALSE
+# init ----
+# This part does not show up in your rendered report, only in the script,
+# because we are using regular comments instead of #' comments
+debug <- 0;
+knitr::opts_chunk$set(echo=debug>-1, warning=debug>0, message=debug>0, class.output="scroll-20", attr.output='style="max-height: 150px; overflow-y: auto;"');
+
+library(ggplot2); # visualisation
+library(GGally);
+library(rio);# simple command for importing and exporting
+library(pander); # format tables
+#library(printr); # set limit on number of lines printed
+library(broom); # allows to give clean dataset
+library(dplyr); #add dplyr library
+library(constellation); #simulated patient data
+
+options(max.print=500);
+panderOptions('table.split.table',Inf); panderOptions('table.split.cells',Inf);
+
+# state global variables at beginning of script
+labs_file <- "labs.csv" 
+orders_file <- "orders.xlsx"
+vitals_file <- "vitals.tsv"
+
+#export data sets to file only relevant for example data from R; real data already on computer or server
+export(constellation::labs, labs_file)
+export(constellation::vitals, vitals_file)
+export(constellation::orders, orders_file)
+
+
+
+# Obtain Data ----
+labs <- import(labs_file) %>% mutate(PAT_ID=paste0('x',PAT_ID))
+vitals <- import(vitals_file) %>% mutate(PAT_ID=paste0('x',PAT_ID))
+orders <- import(orders_file) %>% mutate(PAT_ID=paste0('x',PAT_ID))
+
+#Explore Data ----
+#' # Explore Data
+#' 
+#' ## Labs
+#' 
+#' Summary
+summary(labs) %>% pander()
+#' Data Type
+sapply(labs,class) %>%pander()
+#' Patient Count
+labs$PAT_ID %>% unique() %>% length()
+
+#' ## Orders
+#' 
+#' Summary
+summary(orders) %>%pander()
+#' Data Type
+sapply(orders,class) %>%pander()
+#' Patient Count
+orders$PAT_ID %>% unique() %>% length()
+
+#' ## Vitals
+#' 
+#' Summary
+summary(vitals) %>%pander()
+#' Data Type
+sapply(vitals,class) %>%pander()
+#' Patient Count
+vitals$PAT_ID %>% unique() %>% length()
+
+
+# Intersect and Set Diff Commands----
+#' comparing differences between dataframes. If they're zero, we can rely on "vitals" as source of truth. We want results to be zero. 
+setdiff(labs$PAT_ID,vitals$PAT_ID)
+setdiff(orders$PAT_ID,vitals$PAT_ID)
+setdiff(labs$PAT_ID,orders$PAT_ID)
+setdiff(orders$PAT_ID,labs$PAT_ID)
+
+#' ## How many types of labs and vitals there are
+#' Labs
+labs$VARIABLE %>% table %>% pander
+#' Vitals
+vitals$VARIABLE %>% table %>% pander
+
+
+c()
