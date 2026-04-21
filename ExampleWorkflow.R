792f970d33e96b9dbce388bf5609eaff971744f7
unknown <medela@livemail.uthscsa.edu>
2026-04-21 11:57:24 -0500

    Create Analytic Data Set

--- ExampleWorkflow.R
+++ ExampleWorkflow.R
@@ -22,6 +22,7 @@
 # This part does not show up in your rendered report, only in the script,
 # because we are using regular comments instead of #' comments
 debug <- 0;
+seed <- 130409
 knitr::opts_chunk$set(echo=debug>-1, warning=debug>0, message=debug>0, class.output="scroll-20", attr.output='style="max-height: 150px; overflow-y: auto;"');
 
 library(ggplot2); # visualisation
@@ -32,6 +33,10 @@ library(pander); # format tables
 library(broom); # allows to give clean dataset
 library(dplyr); #add dplyr library
 library(constellation); #simulated patient data
+library(tidymodels)
+library(skimr)
+library(tidyr)
+
 
 options(max.print=500);
 panderOptions('table.split.table',Inf); panderOptions('table.split.cells',Inf);
@@ -100,5 +105,24 @@ vitals$VARIABLE %>% table %>% pander
 #' Optional but highly recommended assignment for next session: Figure out (with an LLM's help wherever necessary, upload your script if you do ask it for help):
  
 "How do I convert the vitals and the labs data frames to ones that have a granularity of patient and day, with separate columns for each type of variables in those respective data frames?"
+
+#' # Create Analytic Data Set
+#'
+set.seed(seed)
+D0 <- bind_rows(labs,vitals) %>% 
+  mutate(RECORDED_TIME = as.Date(RECORDED_TIME)) %>% 
+  pivot_wider(names_from = VARIABLE,values_from = VALUE,values_fn = median) %>%
+  group_by(PAT_ID) %>% 
+  group_initial_split(prop = 0.75,group = PAT_ID)
+
+D1 <- training(D0)
+nrow(D0); nrow(D1)
+skim(D1)
+
+D2 <- group_by(D1,PAT_ID) %>% arrange(RECORDED_TIME) %>% mutate(diff = c(NA,diff(RECORDED_TIME)))
+ggplot(D2,aes(y=diff))+geom_histogram()
+
+## To do: convert dates into number of days since that patient's first day
+
 
 c()
